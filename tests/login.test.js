8a39241bc0850ad3d41c878d85b445d5818211e9
Lucas Pina <ti.lucaspina@gmail.com>
2026-04-30 22:52:36 -0300

    adicionando stages ao teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,8 +2,13 @@ import http from "k6/http";
 import { check, sleep } from "k6";
 
 export const options = {
-  vus: 10,
-  duration: "30s",
+  stages: [
+    { duration: "10s", target: 10 }, // Ramp-up to 10 users over 30 seconds
+    { duration: "20s", target: 10 },  // Stay at 10 users for 1 minute
+    { duration: "10s", target: 30 },
+    { duration: "20s", target: 30 },
+    { duration: "20s", target: 0 }   // Ramp-down to 0 users over 30 seconds
+  ],
   thresholds: {
     http_req_duration: ["p(95)<3000","max<5000"],
     http_req_failed: ["rate<0.1"] 
